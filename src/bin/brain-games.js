f119c81d46d942a7d5161875ec8838293da629a1
Mike Andreev <mike.andrews.1988@gmail.com>
2019-09-01 11:19:41 +0300

    Step 3: add greeting finction and lib readline-sync

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
+import greetings from '..'
 
-console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!\n');
+greetings();
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import readline from 'readline-sync'
+
+const greetings = () => {
+  const playerName = readline.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
+};
+
+export default greetings;
