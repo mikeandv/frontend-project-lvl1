9067693c7157241c1366b15f4a38af6b0a5e2abb
Mike Andreev <mike.andrews.1988@gmail.com>
2019-09-02 22:56:06 +0300

    Step 5: Add logic for even game

---
+++ src/bin/brain-even.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import { evenStart } from '..';
+
+console.log('Welcome to the Brain Games!\n');
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+evenStart();
--- src/index.js
+++ src/index.js
@@ -2,7 +2,36 @@ import readline from 'readline-sync';
 
 const greetings = () => {
   const playerName = readline.question('May I have your name? ');
-  console.log(`Hello, ${playerName}!`);
+  console.log(`Hello, ${playerName}!\n`);
+  return playerName;
 };
 
-export default greetings;
+const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
+const checkCorrectInput = (answer) => answer === 'yes' || answer === 'no';
+const gameStage = 3;
+
+const gameLogic = (stageCount, playerName) => {
+  if (stageCount === 0) {
+    console.log(`Congratulations, ${playerName}!`);
+    return null;
+  }
+  const number = Math.round(Math.random() * 100);
+
+  console.log(`Question: ${number}`);
+  const correctAnswer = isEven(number);
+  const answer = readline.question('Your answer: ');
+
+  if (!checkCorrectInput(answer) || answer !== correctAnswer) {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    console.log(`Let's try again, ${playerName}!`);
+    return null;
+  }
+  console.log('Correct!');
+  return gameLogic(stageCount - 1, playerName);
+};
+
+const evenStart = () => {
+  gameLogic(gameStage, greetings());
+};
+
+export { evenStart, greetings };
