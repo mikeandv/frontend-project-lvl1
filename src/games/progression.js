ceb96818a6ee117428a35b7a5f09e95bec07b3fc
Mike Andreev <mike.andrews.1988@gmail.com>
2019-10-01 22:21:41 +0300

    refactor

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,8 +1,8 @@
 import { cons } from '@hexlet/pairs';
 import {
-  l, cons as consList, head, tail, isEmpty,
+  l, cons as consList, head, tail, isEmpty, reverse,
 } from '@hexlet/pairs-data';
-import runGameLogic from '../core';
+import runGame from '../core';
 import getRandomInRange from '../utils/questionsUtil';
 
 const gameDescription = 'What number is missing in the progression?';
@@ -10,10 +10,10 @@ const progressionStepMin = 2;
 const progressionStepMax = 10;
 const progressionLength = 10;
 
-const bildProgression = (length, progressionStep, first) => {
+const buildProgression = (length, progressionStep, first) => {
   const iter = (item, acc) => {
     if (item === 0) {
-      return acc;
+      return reverse(acc);
     }
     const nextElement = head(acc) + progressionStep;
     return iter(item - 1, consList(nextElement, acc));
@@ -21,35 +21,25 @@ const bildProgression = (length, progressionStep, first) => {
   return iter(length, consList(first, l()));
 };
 
-const getHidenElement = (progression, hidePosition) => {
-  const loop = (list, counter) => {
-    if (counter === hidePosition) {
-      return head(list);
-    }
-    return loop(tail(list), counter + 1);
-  };
-  return loop(progression, 0);
-};
-
-const hideProgressionElement = (progression, hidePosition) => {
-  const loop = (list, counter) => {
+const makeQuestion = (progression, hidePosition) => {
+  const iter = (list, counter) => {
     if (isEmpty(list)) {
       return '';
     }
-    return `${loop(tail(list), counter + 1)} ${counter === hidePosition ? '..' : head(list)}`;
+    return `${counter === hidePosition ? '..' : head(list)} ${iter(tail(list), counter + 1)}`;
   };
-  return loop(progression, 0);
+  return iter(progression, 0);
 };
 
 const makeTask = () => {
   const firstMember = getRandomInRange(1, 100);
   const progressionStep = getRandomInRange(progressionStepMin, progressionStepMax);
-  const hidePosition = getRandomInRange(0, progressionLength - 1);
-  const progression = bildProgression(progressionLength - 1, progressionStep, firstMember);
+  const hideElementPosition = getRandomInRange(0, progressionLength - 1);
+  const progression = buildProgression(progressionLength - 1, progressionStep, firstMember);
 
-  const question = hideProgressionElement(progression, hidePosition);
-  const answer = `${getHidenElement(progression, hidePosition)}`;
+  const question = makeQuestion(progression, hideElementPosition);
+  const answer = (firstMember + (progressionStep * hideElementPosition)).toString();
   return cons(question, answer);
 };
 
-export default () => runGameLogic(makeTask, gameDescription);
+export default () => runGame(makeTask, gameDescription);
